5c1448f635b68d1e75557b4e85edbd407d415f00
Tim Brugman <tim@timbrugman.com>
2022-09-11 02:47:30 +0200

    WIP

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # Decent Looking Emails
 
 Work in progress.
+
+## Emails
+
+https://github.com/johnbillion/wp_mail
+
+### Tested
+
+- (C-07) User or Administrator requests a password reset
+- (C-08) User resets their password
+- (C-09) User changes their password
+- (C-10) User attempts to change their email address
+- (C-11) User changes their email address
+
+### Untested
+
+- (A-01) Comment is awaiting moderation
+- (A-02) Comment is published
+- (B-03) Change of site admin email address is attempted
+- (B-04) Site admin email address is changed
+- (B-05) Change of network admin email address is attempted
+- (B-06) Network admin email address is changed
+- (D-12) Personal data export or erasure request is created or resent
+- (D-13) User confirms personal data export or erasure request
+- (D-14) Site admin sends link to a personal data export
+- (D-15) Site admin erases personal data to fulfill a data erasure request
+- (E-16) Automatic plugin or theme updates
+- (E-17) Automatic core update
+- (E-18) Full log of background update results
+- (F-19) An existing user is invited to a site (MS)
+- (F-20) A new user is invited to join a site (MS)
+- (F-21) A new user account is created (MS)
+- (F-22) A user is added, or their account activation is successful
+- (F-23) A new user is created (x2)
+- (G-24) A new site is created
+- (G-25) User registers for a new site
+- (G-26) User activates their new site (to network admin)
+- (G-27) User activates their new site (to site admin)
+- (H-28) Installation
+- (H-29) A fatal error occurs
+- (H-30) Site admin requests to delete site
--- decent-looking-emails.php
+++ decent-looking-emails.php
@@ -7,6 +7,10 @@ Plugin Name: Decent Looking Emails
 if ( !defined( 'ABSPATH' ) )
     exit;
 
+/**
+ * Dev helpers.
+ */
+
 function d( $var )
 {
     echo "<pre style=\"max-height: 800px; z-index: 9999; position: relative; overflow-y: scroll; white-space: pre-wrap; word-wrap: break-word; padding: 10px 15px; border: 1px solid #fff; background-color: #161616; text-align: left; line-height: 1.5; font-family: Courier; font-size: 16px; color: #fff; \">";
@@ -14,49 +18,41 @@ function d( $var )
     echo "</pre>";
 }
 
+/**
+ * Helpers.
+ */
+
+// function set_wp_mail_content_type( $content_type )
+// {
+//     if ( $content_type == 'text/plain' )
+//         return 'text/html';
+
+//     return $content_type;
+// }
+
 function set_wp_mail_content_type_html()
 {
     return 'text/html';
 }
 
-add_action( 'admin_init', function () {
+// function set_wp_mail_content_type_plain()
+// {
+//     return 'text/plain';
+// }
 
-    if ( $_SERVER['QUERY_STRING'] != 'sendmail' )
-        return;
-
-    d( 'sending some regular ass email' );
-
-    $message = "Line 1.\nLine 2.\n\nLine 3 is longer.\n\nLine 4.\nLine 5.";
-    // $message = "Line 1.\nLine 2.\n\nLine 3 is longer.\n\nLine 4.\nLine 5.";
-
-    // wp_mail(
-    //     'example@example.org', // to
-    //     'test', // subject
-    //     $message, // message
-    // );
-
-    // sleep( 3 );
-
-    wp_mail(
-        'example@example.org', // to
-        'i am the subject', // subject
-        $message, // message
-    );
-
-    d( 'done' );
-});
-
-function use_template( $args )
+function build_html_email_message( $args )
 {
     $html = file_get_contents( __DIR__.'/template-01.html' );
 
     // $args['message'] = htmlentities( $args['message'] );
+    // $args['message'] = preg_replace( '/http(.*)</', '<a href="http$1">http$1</a><', $args['message'] );
+    // $args['message'] = preg_replace( '#<(https?://[^*]+)>#', '$1', $args['message'] );
+    // $args['message'] = nl2br( $args['message'] );
+
+    // make links clickable
+    $args['message'] = make_clickable( $args['message'] );
+    // make paragraphs
     $args['message'] = wpautop( $args['message'] );
-    // UNTESTED
-    // https://github.com/johnbillion/wp_mail
-    // GOOD
-    // to user, password reset
-    $args['message'] = preg_replace( '/http(.*)</', '<a href="http$1">http$1</a><', $args['message'] );
 
     $replacements = [
         '[SUBJECT]'   => $args['subject'],
@@ -68,21 +64,54 @@ function use_template( $args )
     return strtr( $html, $replacements );
 }
 
+/**
+ * Before sending email.
+ */
+
 add_filter( 'wp_mail', function ( $args ) {
-    d( 'intercepting' );
-
-    d( $args );
-
+    // d( 'intercepting' );
+    // d( $args );
     // remove_filter( 'wp_mail_content_type', 'set_wp_mail_content_type_html' );
 
-    // if ( $args['subject'] != 'test' )
-    //     return $args;
-
-    $args['message'] = use_template( $args );
-
     add_filter( 'wp_mail_content_type', 'set_wp_mail_content_type_html' );
+
+    $args['message'] = build_html_email_message( $args );
 
     return $args;
 
 }, 10, 1 );
+
+/**
+ * After sending email.
+ */
+
+// add_action( 'wp_mail_succeeded', function ( $mail_data ) {
+//     d( 'this is wp_mail_succeeded' );
+// });
+
+// add_action( 'wp_mail_failed', function ( $error ) {
+//     d( 'this is wp_mail_failed' );
+// });
+
+/**
+ * (TEMP) Trigger email manually.
+ */
+
+add_action( 'admin_init', function () {
+
+    if ( $_SERVER['QUERY_STRING'] != 'sendmail' )
+        return;
+
+    d( 'sending some regular ass email' );
+
+    $message = "Line 1.\nLine 2.\n\nLine 3 is longer.\n\nLine 4.\nLine 5.";
+
+    wp_mail(
+        'example@example.org', // to
+        'i am the subject', // subject
+        $message, // message
+    );
+
+    d( 'done' );
+});
 
